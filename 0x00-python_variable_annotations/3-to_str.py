4d7657eeda27fef5938d6a7935b51770a321d9f0
mauricioolarte <reneolarte@yahoo.com>
2021-07-19 20:37:09 -0500

    files

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+''' function to_str that takes a float n as
+    argument and returns the string representation of the float.
+'''
+
+
+def to_str(n: int) -> str:
+    '''function to_str that takes a float n as
+    argument and returns the string representation of the float.
+    '''
+    return str(n)
