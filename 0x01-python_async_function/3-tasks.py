1d93f70dc94038e57624b5c180fe4fe08375bb22
mauricioolarte <reneolarte@yahoo.com>
2021-07-21 12:11:33 -0500

    3-tasks.py

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''
+    takes in 2 int arguments (in this order): n
+    and max_delay. You will spawn wait_random n
+    times with the specified max_delay
+'''
+
+import asyncio
+import random
+import time
+from typing import Callable
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> Callable[[float], float]:
+    ''' import a asyncio task '''
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
