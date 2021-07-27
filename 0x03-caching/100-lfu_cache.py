ff3387a08657b69f41ee0e2414c6f1927a672afa
mauricioolarte <reneolarte@yahoo.com>
2021-07-26 22:36:05 -0500

    100-lfu_cache.py

--- 0x03-caching/100-lfu_cache.py
+++ 0x03-caching/100-lfu_cache.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-""" class LFUCache that inherits from BaseCaching and is a caching system """
+""" class LFUCache that inherits from BaseCaching and is a caching system"""
 
 from base_caching import BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """class LFUCache that inherits from BaseCaching and is a caching system """
+    """ class LFUCache that inherits from BaseCaching and is a caching system """
 
     def __init__(self):
         """ superconstructor """
         super().__init__()
         self.cacheList = []
-        self.counter = {}
+        self.elements = {}
 
     def put(self, key, item):
         """ Puts item in cache """
@@ -20,52 +20,50 @@ class LFUCache(BaseCaching):
 
         self.cache_data[key] = item
 
-        cuantity = self.counter.get(key, None)
+        item_count = self.elements.get(key, None)
 
-        if cuantity is not None:
-            self.counter[key] += 1
+        if item_count is not None:
+            self.elements[key] += 1
         else:
-            self.counter[key] = 1
+            self.elements[key] = 1
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first = self.firstItemList(self.cacheList)
+            first = self.firstElementlist(self.cacheList)
             if first:
                 self.cacheList.pop(0)
                 del self.cache_data[first]
-                del self.counter[first]
+                del self.elements[first]
                 print("DISCARD: {}".format(first))
 
         if key not in self.cacheList:
             self.cacheList.insert(0, key)
-        self.moveStartlist(key)
+        self.mv_right_list(key)
 
     def get(self, key):
         """ Gets item from cache """
         item = self.cache_data.get(key, None)
         if item is not None:
-            self.counter[key] += 1
-            self.moveStartlist(key)
+            self.elements[key] += 1
+            self.mv_right_list(key)
         return item
 
-    def moveStartlist(self, item):
-        """  """
+    def mv_right_list(self, item):
+        """ Moves element to the right, taking into account LFU """
         length = len(self.cacheList)
 
         position = self.cacheList.index(item)
-        cuantity = self.counter[item]
+        item_count = self.elements[item]
 
         for i in range(position, length):
             if i != (length - 1):
                 next = self.cacheList[i + 1]
-                rest = self.counter[next]
+                next_count = self.elements[next]
 
-                if rest > cuantity:
+                if next_count > item_count:
                     break
 
         self.cacheList.insert(i + 1, item)
         self.cacheList.remove(item)
 
     @staticmethod
-    def firstItemList(array):
-        """ Get first element of list"""
-        return array[0] if array else None
+    def firstElementlist(array):
