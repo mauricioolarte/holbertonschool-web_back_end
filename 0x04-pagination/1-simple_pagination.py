3cbc5a360c7ecc32661dda47129d41463069540c
mauricioolarte <reneolarte@yahoo.com>
2021-07-28 19:46:49 -0500

    1-simple_pagination.py

---
+++ 0x04-pagination/1-simple_pagination.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+'''
+named index_range that takes two integer arguments page and page_size
+'''
+
+
+from typing import List
+import math
+import csv
+
+
+def index_range(page, page_size):
+    '''  takes two integer arguments page and page_size
+        return a tuple of size two containing a start index and an end
+        index corresponding '''
+    return ((page - 1)*page_size, (page*page_size))
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        resp = []
+        assert isinstance(page, int) is True and isinstance(page_size) is True,
+        "values must be int"
+        assert page > 0 and page_size > 0, "Value must be positive"
+        ranges = index_range(page, page_size)
+        values = self.dataset()
+        max_range = len(values)
+        if ranges[0] > max_range or ranges[1] > max_range:
+            return []
+        min_limit = ranges[0]
+        max_range = ranges[1]
+        print(type(min_limit))
+        for value in range(min_limit, max_range):
+            resp.append(values[value])
+        return resp
