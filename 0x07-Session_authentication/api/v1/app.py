8a4d7fa0157aa1294378e9d8282fcc4e7aba891c
mauricioolarte <reneolarte@yahoo.com>
2021-08-10 22:37:49 -0500

    files

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -66,6 +66,7 @@ def before_request() -> str:
 
     if auth.authorization_header(request) is None and \
             auth.session_cookie(request) is None:
+        abort(401)
 
     if auth.authorization_header(request) is None:
         abort(401)
