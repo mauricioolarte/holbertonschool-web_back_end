012615f77959517aed90fbf789d310aac5282fa8
mauricioolarte <reneolarte@yahoo.com>
2021-09-07 20:36:43 -0500

    12-log_stats.py

--- 0x0D-NoSQL/12-log_stats.py
+++ 0x0D-NoSQL/12-log_stats.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
-""" Log stats """
+""" MongoDB Operations with Python using pymongo """
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-    """ provides some stats about Nginx logs stored in MongoDB """
+    """ Provides some stats about Nginx logs stored in MongoDB """
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
+
+    # get_documents = nginx_collection.aggregate(
+    #     [
+    #         {"$match": {"method": "GET"}},
+    #         {"$count": "gets"}
+    #     ]
+    # )
 
     n_logs = nginx_collection.count_documents({})
     print(f'{n_logs} logs')
