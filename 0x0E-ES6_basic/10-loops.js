19c397bd060b625f74b2ac6104f1e68a8f0e0423
mauricioolarte <reneolarte@yahoo.com>
2021-09-09 21:55:55 -0500

    10-loops.js

---
+++ 0x0E-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
+  for (const elem of array) {
+    newArray.push(appendString + value);
+  }
+
+  return newArray;
+}
