84da274213dd707c2fd290e2bc069f76862637e1
mauricioolarte <reneolarte@yahoo.com>
2021-09-09 21:43:46 -0500

    5-spread-operator.js

--- 0x0E-ES6_basic/5-spread-operator.js
+++ 0x0E-ES6_basic/5-spread-operator.js
@@ -1,3 +1,3 @@
-export default function concatArrays(arr1, arr2, string) {
-  return [...arr1, ...arr2, ...string];
+export default function concatArrays(array1, array2, string) {
+  return [...array1, ...array2, ...string];
 }
